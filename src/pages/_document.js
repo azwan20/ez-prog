484db47b2520bf5fc1968ca1ae0ce09de43544ed
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2023-11-07 02:04:04 +0800

    Add files via upload

---
+++ src/pages/_document.js
@@ -0,0 +1,33 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      {/* <!-- boostrap --> */}
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+      </script>
+
+      {/* Flowbite */}
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css" rel="stylesheet" />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
+      {/* fgoogle font */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"></link>
+      <Head>
+        <title>EZprog</title>
+        {/* <img src='/logo.svg' /> */}
+        <link rel="icon" href="/logo-warna.svg" />
+        <meta name="description" content="Place the meta description text here." />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
