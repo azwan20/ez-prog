dcfd04154ed568ca21e1b860223a81b1acf14e86
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2023-11-07 22:39:45 +0800

    Update header.js

--- src/pages/landingPage/header.js
+++ src/pages/landingPage/header.js
@@ -1,17 +1,28 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header() {
+    const [menuVisible, setMenuVisible] = useState(false); // State untuk mengontrol tampilan menu
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
     return (
         <>
             <nav>
                 <div>
                     <img src="/logoNav.svg" />
                 </div>
-                <div>
-                    <Link href="">Home</Link>
-                    <Link href="">Service</Link>
-                    <Link href="">Portofolio</Link>
-                    <Link href="">About</Link>
+                <div className="menu">
+                    <button className="burger" onClick={toggleMenu}>
+                        <span class="material-symbols-outlined">menu</span>
+                    </button>
+                    <div className={`links ${menuVisible ? "active" : ""}`}>
+                        <Link href="">Home</Link>
+                        <Link href="">Service</Link>
+                        <Link href="">Portofolio</Link>
+                        <Link href="">About</Link>
+                    </div>
                 </div>
             </nav>
 
