10d4866f6e651864200bdcd50719a0afea4026cb
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2023-11-11 19:38:32 +0800

    Update index.js

--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -1,9 +1,6 @@
 import Link from "next/link";
 import Footer from "./footer";
 import Header from "./header";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from "react";
 
 export default function Landingpage() {
     const svg = ["mobile.svg", "web.svg", "ml.svg", "ui.svg", "design.svg", "proposalskripsi.svg"];
@@ -30,10 +27,6 @@ export default function Landingpage() {
     ];
     const aboutsvg = ["Kontrak-kerja.svg", "Design-about.svg", "website.svg", "ml-about.svg", "Mobile-about.svg", "Proposal-Skripsi.svg"];
     const aboutItem = ["Kontrak Kerja", "Design", "Web Application", "Machine Learning", "Mobile Application", "Proposal/Skripsi"];
-
-    useEffect(() => {
-        AOS.init(); // Inisialisasi AOS
-    }, []);
     return (
         <>
             <main>
