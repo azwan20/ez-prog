5103f64752c1f8fff61b0152e439f937de3db87b
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2023-11-11 19:00:56 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
 import '@/styles/globals.css'
 import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import AOS from 'aos';
 
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    // Inisialisasi AOS pada komponen did mount
+    AOS.init({
+      // Anda dapat menambahkan konfigurasi AOS di sini jika diperlukan
+      duration: 1000, // Contoh: durasi animasi 1 detik
+    });
+  }, []); // []
+  
   return <Component {...pageProps} />
 }
